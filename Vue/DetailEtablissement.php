a45e197877a8d978ea60e957ae5e3263ffeb02c9
D0ria <doriaremadna@gmail.com>
2023-04-14 14:53:50 +0200

    page détail sans données ok

--- Vue/DetailEtablissement.php
+++ Vue/DetailEtablissement.php
@@ -1,10 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Vue/css/DetailEtablissement.css">
+</head>
+<body>
 <div>
-    <h1>Detail de l'établissement <?= $value['Nom'] ?> </h1>
-            <div class="vignette">
-            <fieldset>
-                <?= $value['Nom'] ?> | <?= $value['Adresse'] ?><br>
-                <?=  $value['Type'] ?> | <?=  $value['Notemoyenne'] ?><br>
-                <?=  $value['Prixmoyen'] ?>  € <br>
-            </fieldset>
+    <h1>Detail de l'établissement  </h1>
+        <div class="vignette">
+            <div class = "left">
+                <div class = "image"><p>img</p></div>
+                <div class = "nom"><p>name</p></div>
             </div>
-    </div>
+            <div class="right">
+                <div class = "type"><p>type</p></div>
+                <div class = "note"><p>note</p></div>
+                <div class = "prix"><p>prix</p></div>
+                <div class = "adresse"><p>adr</p></div>
+            </div>
+            
+        </div>
+</div>
+</body></html>
